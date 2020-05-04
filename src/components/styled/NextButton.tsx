45578ce78ac297adee79bcbbe16d74cb0f33e66a
Jack <jackwoodpearce@gmail.com>
2020-05-04 23:07:01 +0100

    Add eslint and prettier config files

---
+++ src/components/styled/NextButton.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const NextButton = styled.button`
+    background-color: #55ff44;
+    font-size: 34px;
+`;
+
+export default NextButton
---
+++ src/components/styled/Title.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+color: red;
+font-family: Arial, Helvetica, sans-serif;
+border: 2px solid blue;
+border-radius: 4px;
+`;
+
+export default Title;
