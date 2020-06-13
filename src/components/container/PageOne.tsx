16ce617efd684fc0423beeb8f46087613c9b8a25
Jack <jackwoodpearce@gmail.com>
2020-06-14 00:05:31 +0100

    Create a link btn matching bootstrap styling

--- src/components/container/PageOne.tsx
+++ src/components/container/PageOne.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import LinkButton from '../styled/LinkButton'
 
 export default function PageOne() {
     return (
         <div>
             <h1>PAGE ONE</h1>
+            <LinkButton link={'/'} linkName={'HOME'}/>
         </div>
     )
 }
--- src/components/styled/NextButton.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const NextButton = styled.button`
-    background-color: #55ff44;
-    font-size: 34px;
-`;
-
-export default NextButton
