b07757336adf59fb34ef95b2b6334716be753f72
Jack <jackwoodpearce@gmail.com>
2020-06-15 09:04:56 +0100

    Add ability to change the global theme on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, ChangeEvent } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './styles/global';
-// import { theme1, theme2 } from './styles/theme';
+import { theme1, theme2 } from './styles/theme';
 
 import Header from './components/container/Header';
 import NavBar from './components/styled/NavBar';
@@ -11,25 +11,36 @@ import PageOne from './components/container/PageOne';
 import PageTwo from './components/container/PageTwo';
 import Footer from './components/container/Footer';
 
+interface AppState {
+        theme: {
+            theme_background_color: string;
+            theme_color: string;
+            brand_color_blue: string;
+            brand_color_teal: string;
+            brand_color_magenta: string;
+        }
+}
+
 class App extends Component {
 
-    // state = {
-    //     theme: theme1
-    // }
+    state = {
+        theme: theme1
+    }
 
-    // handleThemeChange = (e:any) => {
-    //   let theme = e.target.value
-    //   theme === 'theme1' ? (theme = theme1) : (theme = theme2)
-    //   this.setState({ theme })
-    // }
+    handleThemeChange = (e:ChangeEvent<HTMLInputElement>):void => {
+        let theme: {};
+        e.target.checked ? (theme = theme1) : (theme = theme2)
+        this.setState({ theme })
+    }
 
     render() {
         return (
-            // <ThemeProvider theme={theme}>
+            <ThemeProvider theme={this.state.theme}>
                 <>
                     <GlobalStyles />
                     <Router>
                         <Header />
+                        <input type='checkbox' onChange={this.handleThemeChange} />
                         <NavBar />
                         <Switch>
                             <Route exact={true} path="/" component={Home} />
@@ -40,7 +51,7 @@ class App extends Component {
                         <Footer />
                     </Router>
                 </>
-            // </ThemeProvider>
+            </ThemeProvider>
         );
     }
 };
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,15 @@
 
-export const theme = {
+export const theme1 = {
+    theme_background_color: '#fff',
+    theme_color: '#000',
+    brand_color_blue: '#0e1c75',
+    brand_color_teal: '#00ccc6',
+    brand_color_magenta: '#e21266',
+};
+
+export const theme2 = {
+    theme_background_color: '#000',
+    theme_color: '#fff',
     brand_color_blue: '#0e1c75',
     brand_color_teal: '#00ccc6',
     brand_color_magenta: '#e21266',
