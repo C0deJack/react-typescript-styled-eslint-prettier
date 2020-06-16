016647723da8330e9d68361ec014fda3770f6f3b
Jack <jackwoodpearce@gmail.com>
2020-06-16 21:46:19 +0100

    Moved functionally to themetoggle and styled the button

--- src/components/container/Header.tsx
+++ src/components/container/Header.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Logo from '../styled/Logo';
-// import ThemeToggle from '../styled/ThemeToggle';
+import ThemeToggle from '../styled/ThemeToggle';
 import SearchBar from '../styled/SearchBar';
-import { Theme, lightTheme, darkTheme } from '../../styles/theme';
+import { Theme } from '../../styles/theme';
 
 const StyledHeader = styled.header`
     display: flex;
@@ -17,22 +17,12 @@ type HeaderProps = {
 
 export default function Header(props: HeaderProps) {
 	
-	// const changeTheme = (e:ChangeEvent<HTMLInputElement>):void => {
-	const changeTheme = (e:any):void => {
-		if(e.target.checked) {
-			props.setTheme(darkTheme);
-		} else {
-			props.setTheme(lightTheme);
-		}
-	}
-
     return (
         <StyledHeader>
             <Logo />
             <div>
                 <SearchBar />
-                {/* <ThemeToggle /> */}
-				<input type="checkbox" value='true' onClick={changeTheme}/>
+                <ThemeToggle setTheme={props.setTheme} />
             </div>
         </StyledHeader>
     );
