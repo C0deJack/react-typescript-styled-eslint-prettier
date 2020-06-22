7cd61852cffe2131685eea0e401e1c2f1387ceaa
Jack <jackwoodpearce@gmail.com>
2020-06-22 23:04:43 +0100

    Add Autocomplete input component to project

--- src/components/container/PageOne.tsx
+++ src/components/container/PageOne.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import LinkButton from '../styled/LinkButton'
-
+import React from 'react';
+import LinkButton from '../styled/LinkButton';
+import Autocomplete from '../styled/Autocomplete';
 export default function PageOne() {
     return (
         <div>
             <h1>PAGE ONE</h1>
+
+            <Autocomplete
+                suggestions={[
+                "Alligator",
+                "Bask",
+                "Crocodilian",
+                "Death Roll",
+                "Eggs",
+                "Jaws",
+                "Reptile",
+                "Solitary",
+                "Tail",
+                "Wetlands"
+            ]}
+            />
+
+
+
             <LinkButton link={'/'} linkName={'HOME'}/>
         </div>
     )
